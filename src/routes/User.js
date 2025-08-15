2a1e22138d3fcc60c6336b479ea8e8d6c8da2601
Nitesh Kumar <kumar183nitesh@gmail.com>
2025-08-16 03:33:00 +0530

    Fix import statement for User model and add user authentication routes

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,10 +1,34 @@
 import express from 'express';
-import { registerController } from '../controller/User.js';
+import {  registerController } from '../controller/User.js';
 import { loginController } from '../controller/User.js';
+import {requireSignIn, isAdmin } from '../middlewares/Auth.js'
 
 const app = express.Router();
 
 app.post('/register', registerController );
 app.post('/login', loginController ); // Assuming you have a loginController
+// router.get('/user', getUserInfo);
+
+//protected User route auth
+   app.get('/user-auth', requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+// //protected Admin route auth
+   app.get('/is-admin', requireSignIn , isAdmin  , (req, res) => {
+    res.status(200).send({ ok: true });
+});
 
 export default app;
+
+// import { getUserInfo, loginController, registerController } from "../controller/User.js";
+// import { requireSignIn, isAdmin } from "../middlewares/Auth.js";
+
+// const router = express.Router();
+
+// router.post("/register", registerController);
+// router.post("/login", loginController);
+// 
+
+// 
+
+// export default router;
