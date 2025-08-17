a6c3dfe68e4c4a78646d26bf87e16c2195124943
Nitesh Kumar <kumar183nitesh@gmail.com>
2025-08-17 05:41:50 +0530

    Implement category update and delete functionality, and enhance single category retrieval with associated posts

--- src/controller/Category.js
+++ src/controller/Category.js
@@ -1,6 +1,7 @@
 
 import CategoryModel from "../models/Category.js";
 import slug from "slugify";
+import postModel from "../models/Post.js";
 export const createCategoryController= async(req, res) => {
 
     try {
@@ -64,3 +65,70 @@ export const getAllCategory = async (req, res)=>{
   }
 
 }
+
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+    const category = await categoryModel.findByIdAndUpdate(
+      id,
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      messsage: "Category Updated Successfully",
+      category,
+    });
+  } catch (error) {
+    // console.log(error);
+    return res.status(500).send({
+      success: false,
+      error,
+      message: "Error while updating category",
+    });
+  }
+};
+
+//delete category
+export const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await CategoryModel.findByIdAndDelete(id);
+    return res.status(200).send({
+      success: true,
+      message: "Categry Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return  res.status(500).send({
+      success: false,
+      message: "error while deleting category",
+      error,
+    });
+  }
+};
+
+// single category
+export const singleCategoryController = async (req, res) => {
+  try {
+    const category = await CategoryModel.findOne({ slug: req.params.slug });
+    const post = await postModel.find({category}).populate("category");
+    return res.status(200).send({
+      success: true,
+      message: "Get SIngle Category SUccessfully",
+      category,
+      post,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While getting Single Category",
+    });
+  }
+};
+
+
+
--- src/routes/Category.js
+++ src/routes/Category.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createCategoryController, getAllCategory } from "../controller/Category.js";
-
+import { createCategoryController, deleteCategory, getAllCategory, singleCategoryController, updateCategoryController } from "../controller/Category.js";
+import {isAdmin, requireSignIn} from '../middlewares/Auth.js'
 
 const app= express.Router();
 
-app.post("/create-category" , createCategoryController);
-app.get('/get-category', getAllCategory );
-
+app.post("/create-category" ,isAdmin, requireSignIn,  createCategoryController);
+app.get('/get-category',isAdmin, requireSignIn, getAllCategory );
+app.put('/update-category/:id', isAdmin, requireSignIn, updateCategoryController )
+app.delete('/delete-category/:id',isAdmin, requireSignIn,deleteCategory )
+app.get('/single-category/:slug',singleCategoryController )
 
 export default app;
